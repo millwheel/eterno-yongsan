b8f0c3859340724ef4d39becb227049635b9115b
sijun <sionwer5@gmail.com>
2025-10-01 21:13:28 +0900

    feat: about page 배경화면 추ㅏ

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+
+
 export default function AboutPage() {
     return (
         <div className="flex items-center justify-center min-h-screen">
+            <Image
+                src="/image/about.jpg"
+                alt="Background"
+                fill
+                priority
+                className="object-cover -z-10"
+            />
+
             <div className="flex-col items-center justify-center">
-                <p>사업개요 표 추가</p>
+                <p className="text-white bg-gray-700/70 font-bold text-4xl">사업개요 표 여기에 추가</p>
             </div>
         </div>
     );
