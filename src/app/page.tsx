323152c1bc1379cde5c0fd87a1dfc35e6e69ef86
sijun <sionwer5@gmail.com>
2025-11-02 18:25:06 +0900

    fix: 인덱싱 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,25 @@ import Link from "next/link";
 import {ChevronDown} from "lucide-react";
 import Image from "next/image";
 
+const career: [string, string][] = [
+    ["1966", "마드리드대학교 건축학과 교수"],
+    ["1970", "프린스턴대학교, 하버드대학교 스위스 로잔대학교 방문교수"],
+    ["1972–1980", "바르셀로나 공과대학교 교수"],
+    ["1980–1984", "마드리드 공과대학교 교수"],
+    ["1985–1990", "하버드대학교 건축대학원 학과장"],
+    ["1998", "스페인 왕립 예술 아카데미 회원"],
+    ["2013", "미국 예술 아카데미 명예 회원"],
+];
+
+const awards: [string, string][] = [
+    ["1993", "롤프쇼크 상 수상"],
+    ["1996", "프리츠커 상 수상"],
+    ["2001", "미스 반 데 로에 현대건축상 수상"],
+    ["2003", "영국왕립건축가협회 로열 골드 메달 수상"],
+    ["2012", "토마스 제퍼슨 메달 건축 부문 수상"],
+    ["2012", "프린스페 데 아스투리아스 상 수상 외 다수"],
+];
+
 export default function Home() {
     return (
         <div>
@@ -75,28 +94,20 @@ export default function Home() {
                 {/* 오른쪽 하단 텍스트 */}
                 <div className="absolute bottom-20 right-20 z-10 text-white text-left leading-relaxed space-y-5">
                     {/* 이름 및 소개 */}
-                    <h2 className="text-4xl font-semibold tracking-widest">Rafel Moneo</h2>
+                    <h2 className="text-4xl tracking-widest">Rafel Moneo</h2>
                     <p className="text-sm opacity-90">
                         1937년 5월 9일, 스페인 출생<br />
                         마드리드 공과대학교 건축학 학사
                     </p>
 
                     {/* 경력 및 수상 */}
-                    <div className="flex justify-start gap-10 text-sm opacity-90 font-light">
-                        {/* Reputations */}
+                    <div className="hidden md:flex justify-start gap-10 text-sm opacity-90 font-light">
+                        {/* Career */}
                         <div>
                             <p className="font-semibold mb-3 tracking-wide">Reputations</p>
                             <ul className="space-y-1 leading-relaxed">
-                                {[
-                                    ["1966", "마드리드대학교 건축학과 교수"],
-                                    ["1970", "프린스턴대학교, 하버드대학교 스위스 로잔대학교 방문교수"],
-                                    ["1972–1980", "바르셀로나 공과대학교 교수"],
-                                    ["1980–1984", "마드리드 공과대학교 교수"],
-                                    ["1985–1990", "하버드대학교 건축대학원 학과장"],
-                                    ["1998", "스페인 왕립 예술 아카데미 회원"],
-                                    ["2013", "미국 예술 아카데미 명예 회원"],
-                                ].map(([year, text], idx) => (
-                                    <li key={`${year}-${idx}`} className="flex gap-6">
+                                {career.map(([year, text], idx) => (
+                                    <li key={`${idx}`} className="flex gap-6">
                                         <span className="w-20 text-left font-medium">{year}</span>
                                         <span className="flex-1">{text}</span>
                                     </li>
@@ -108,15 +119,8 @@ export default function Home() {
                         <div>
                             <p className="font-semibold mb-3 tracking-wide">Awards</p>
                             <ul className="space-y-1 leading-relaxed">
-                                {[
-                                    ["1993", "롤프쇼크 상 수상"],
-                                    ["1996", "프리츠커 상 수상"],
-                                    ["2001", "미스 반 데 로에 현대건축상 수상"],
-                                    ["2003", "영국왕립건축가협회 로열 골드 메달 수상"],
-                                    ["2012", "토마스 제퍼슨 메달 건축 부문 수상"],
-                                    ["2012", "프린스페 데 아스투리아스 상 수상 외 다수"],
-                                ].map(([year, text], idx) => (
-                                    <li key={`${year}-${idx}`} className="flex gap-6">
+                                {awards.map(([year, text], idx) => (
+                                    <li key={`${idx}`} className="flex gap-6">
                                         <span className="w-20 text-left font-medium">{year}</span>
                                         <span className="flex-1">{text}</span>
                                     </li>
