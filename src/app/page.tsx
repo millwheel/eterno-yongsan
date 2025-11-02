b6d97714a36c166c7b30b9558fab5607480a12d8
sijun <sionwer5@gmail.com>
2025-11-02 13:45:23 +0900

    feat: 홈화면 디자인 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,56 @@
 import React from "react";
-import CallButton from "@/components/call";
-
-const callInfos = [
-    { label: "에테르노 청담", phoneNumber: "010-1111-2222" },
-    { label: "에테르노 압구정", phoneNumber: "010-3333-4444" },
-    { label: "에테르노 용산", phoneNumber: "010-5555-6666" },
-];
+import Link from "next/link";
 
 export default function Home() {
     return (
         <div>
-            {/* Background Video */}
-            <video
-                className="absolute top-0 left-0 w-full h-full object-cover"
-                autoPlay
-                muted
-                loop
-                playsInline
-            >
-                <source src="/video/main.mp4" type="video/mp4" />
-                브라우저가 동영상을 지원하지 않습니다.
-            </video>
-            <div className="min-h-screen flex items-center justify-center">
-                {/* Overlay (버튼 컨텐츠) */}
-                <div className="flex flex-wrap items-center justify-center gap-8 md:gap-12 translate-y-40">
-                    {callInfos.map((info) => (
-                        <CallButton
-                            key={info.label}
-                            label={info.label}
-                            phoneNumber={info.phoneNumber}
-                        />
-                    ))}
+            {/* Intro section */}
+            <section className="relative w-full h-screen flex items-center justify-center overflow-hidden">
+                {/* Background Video */}
+                <video
+                    className="absolute top-0 left-0 w-full h-full object-cover"
+                    autoPlay
+                    muted
+                    loop
+                    playsInline
+                >
+                    <source src="/video/main.mp4" type="video/mp4" />
+                </video>
+
+                <div className="absolute inset-0 bg-black/30" />
+
+                {/* Center Content */}
+                <div className="relative z-10 flex flex-col items-center text-white text-center gap-20">
+                    <h1 className="flex flex-col items-center justify-center">
+                        <span className="text-6xl ">E T E R N O</span>
+                        <span className="my-4 w-80 h-[1px] bg-white/50" />
+                        <span className="text-4xl tracking-widest">YONGSAN</span>
+                    </h1>
+
+                    <div className="mt-10 flex flex-col md:flex-row gap-10">
+                        <Link
+                            href="/enroll"
+                            className="px-8 py-3 bg-white/50 text-lg text-[#5e5555] rounded-3xl backdrop-blur-sm hover:bg-white transition"
+                        >
+                            관심고객등록
+                        </Link>
+                        <Link
+                            href="/gallery"
+                            className="px-8 py-3 bg-white/50 text-lg text-[#5e5555] rounded-3xl backdrop-blur-sm hover:bg-white transition"
+                        >
+                            갤러리방문예약
+                        </Link>
+                    </div>
                 </div>
-            </div>
+            </section>
+            {/* Rafel Moneo Section */}
+            <section>
+
+            </section>
+            {/* Rafel Moneo Section */}
+            <section>
+
+            </section>
         </div>
     );
 }
